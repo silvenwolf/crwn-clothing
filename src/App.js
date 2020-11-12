91b7de5950d9be33e27df23d91a44052a752cc3c
Sandra Andersen <sandraandersen@geotab.com>
2020-11-12 16:19:20 -0500

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -7,25 +7,36 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
   constructor() {
     super();
 
-    this.state= {
+    this.state = {
       currentUser: null
     }
   }
 
-unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({currentUser: userAuth});
     })
   }
 
@@ -36,7 +47,7 @@ unsubscribeFromAuth = null;
   render() {
     return (
       <div>
-        <Header currentUser = {this.state.currentUser} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
