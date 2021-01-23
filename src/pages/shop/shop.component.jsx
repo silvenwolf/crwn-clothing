a8e5b808be97e13bdcafbc7df3fb5dd9151de8ae
Sandra Andersen <sandraandersen@geotab.com>
2021-01-22 19:30:18 -0500

    promise pattern stuff

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,12 +28,6 @@ class ShopPage extends React.Component {
             updateCollections(collectionsMap);
             this.setState({ loading: false });
         });
-
-        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        //     updateCollections(collectionsMap);
-        //     this.setState({ loading: false });
-        // });
     }
 
     render() {
